1f3101e3c4200eb45c0269e1f6d7db2a855df1af
Dante Healy <dantephealy@gmail.com>
2021-06-05 12:57:22 +0100

    Added home app and templates

--- myshop/settings.py
+++ myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -55,10 +56,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'myshop.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
